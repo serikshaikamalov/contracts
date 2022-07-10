f993ebc944ec4b78f60b80973b2e6e525359e89a
Travis Reeder <treeder@gmail.com>
2022-07-10 05:32:06 +0000

    First draft of erc721 contract

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # contracts
+
 Smart contracts for Nuqtah platform.
+
+## Compile
+
+```sh
+npx hardhat compile
+```
+
+To get bin and ABI, after `compile`, open `artifacts/contracts/contractname.json` and:
+
+* Grab `bytecode` and save it to a contractname.bin
+* Grab `abi` and save it to contractname.abi
---
+++ contracts/ERC721v1.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract ERC721v1 is Context, Pausable, AccessControl, ERC721Burnable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    string private _baseURIInternal;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseURI
+    ) ERC721(name, symbol) {
+        _baseURIInternal = baseURI;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURIInternal;
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
