c590a4def7eeea89eaa466d2ef98a3c638e44915
Serik <serik@Seriks-MacBook-Air.local>
2023-11-22 16:22:15 +0600

    erc20 token contructor updated

--- contracts/ERC20/erc20.sol
+++ contracts/ERC20/erc20.sol
@@ -3,64 +3,93 @@ pragma solidity ^0.8.17;
 
 interface ERC20Interface {
     function totalSupply() external view returns (uint);
+
     function balanceOf(address tokenOwner) external view returns (uint balance);
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+
+    function allowance(
+        address tokenOwner,
+        address spender
+    ) external view returns (uint remaining);
+
     function transfer(address to, uint tokens) external returns (bool success);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
- 
+
+    function approve(
+        address spender,
+        uint tokens
+    ) external returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint tokens
+    ) external returns (bool success);
+
     event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint tokens
+    );
 }
 
 contract IcecreamToken is ERC20Interface {
     string public symbol;
-    string public  name;
+    string public name;
     uint8 public decimals;
     uint public _totalSupply;
- 
+
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
- 
-    constructor() {
-        symbol = "ICT";
-        name = "IceCream Token";
+
+    constructor(string memory n, string memory s) {
+        symbol = n;
+        name = s;
         decimals = 2;
         _totalSupply = 100000;
         balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
- 
+
     function totalSupply() public view returns (uint) {
-        return _totalSupply  - balances[address(0)];
+        return _totalSupply - balances[address(0)];
     }
- 
+
     function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
- 
+
     function transfer(address to, uint tokens) public returns (bool success) {
         balances[msg.sender] = balances[msg.sender] - tokens;
         balances[to] = balances[to] + tokens;
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
- 
-    function approve(address spender, uint tokens) public returns (bool success) {
+
+    function approve(
+        address spender,
+        uint tokens
+    ) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
- 
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+
+    function transferFrom(
+        address from,
+        address to,
+        uint tokens
+    ) public returns (bool success) {
         balances[from] = balances[from] - tokens;
         allowed[from][msg.sender] = allowed[from][msg.sender] - tokens;
         balances[to] = balances[to] + tokens;
         emit Transfer(from, to, tokens);
         return true;
     }
- 
-    function allowance(address owner, address spender) public view returns (uint remaining) {
+
+    function allowance(
+        address owner,
+        address spender
+    ) public view returns (uint remaining) {
         return allowed[owner][spender];
     }
 }
